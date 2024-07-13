6b39acb0b3c6fb9ccfcbd4c033c640e162d5fe14
Shoresh613 <mikael.folkesson@gmail.com>
2024-07-13 23:08:10 +0200

    fix show local time, version bump

--- astro_script.py
+++ astro_script.py
@@ -2698,11 +2698,11 @@ def main(gui_arguments=None):
         string_davison = f"{br}{bold}Davison chart of:{nobold} {', '.join(args['Davison'])}. Stored as new event: {args['Name']}"
     elif args["Davison"]:
         string_davison = f"{br}{bold}Davison chart of:{nobold} {', '.join(args['Davison'])}" + " (not stored, --name lacking)" if not EPHE else ""
-    string_local_time = f"{br}{bold}Local Time:{nobold} {str(local_datetime).strip('0')}" + " LMT" if args["LMT"] else f" {local_timezone}"
-    string_UTC_Time_imprecise = f"{br}{bold}UTC Time:{nobold} {str(utc_datetime).strip('0')} UTC (imprecise due to exact time of day missing)"
+    string_local_time = (f"{br}{bold}Local Time:{nobold} {str(local_datetime).lstrip('0')}" + " LMT") if args["LMT"] else (f"{br}{bold}Local Time:{nobold} {str(local_datetime).strip('0')} {local_timezone}")
+    string_UTC_Time_imprecise = f"{br}{bold}UTC Time:{nobold} {str(utc_datetime).lstrip('0')} UTC (imprecise due to exact time of day missing)"
     degree_symbol = "Delta" if (os.name == 'nt' and output_type == 'html') else "Δ"
     
-    string_UTC_Time = f"{br}{bold}UTC Time:{nobold} {str(utc_datetime).strip('0')} UTC ({degree_symbol}-T adjusted)" 
+    string_UTC_Time = f"{br}{bold}UTC Time:{nobold} {str(utc_datetime).lstrip('0')} UTC ({degree_symbol}-T adjusted)" 
     if notime:
         string_ruled_by = f"{br}{bold}Weekday:{nobold} {weekday} {bold}Day ruled by:{nobold} {ruling_day}"
     else:
